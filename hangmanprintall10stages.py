e9ae8540fa3e39416cc04a898cec2b8a099b1dad
Sam Hawkens <ttv.mogwaian@gmail.com>
2019-04-20 21:28:51 -0400

    Adjusted drawings for hangman and adjusted title. Created program that prints all 10 stages.

---
+++ hangmanprintall10stages.py
@@ -0,0 +1,61 @@
+import time
+
+with open('hangmanstage1') as f:
+    for line in f:
+        line = line.strip('\n')
+        print(line)
+time.sleep(2)
+
+with open('hangmanstage2') as f:
+    for line in f:
+        line = line.strip('\n')
+        print(line)
+time.sleep(2)
+
+with open('hangmanstage3') as f:
+    for line in f:
+        line = line.strip('\n')
+        print(line)
+time.sleep(2)
+
+with open('hangmanstage4') as f:
+    for line in f:
+        line = line.strip('\n')
+        print(line)
+time.sleep(2)
+
+with open('hangmanstage5') as f:
+    for line in f:
+        line = line.strip('\n')
+        print(line)
+time.sleep(2)
+
+with open('hangmanstage6') as f:
+    for line in f:
+        line = line.strip('\n')
+        print(line)
+time.sleep(2)
+
+with open('hangmanstage7') as f:
+    for line in f:
+        line = line.strip('\n')
+        print(line)
+time.sleep(2)
+
+with open('hangmanstage8') as f:
+    for line in f:
+        line = line.strip('\n')
+        print(line)
+time.sleep(2)
+
+with open('hangmanstage9') as f:
+    for line in f:
+        line = line.strip('\n')
+        print(line)
+time.sleep(2)
+
+with open('hangmanstage10') as f:
+    for line in f:
+        line = line.strip('\n')
+        print(line)
+time.sleep(2)
